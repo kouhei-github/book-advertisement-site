1eeaa62add38a7fe54ea5ba06542df9f5e6e076e
kouhei-github <kohei0801nagamatsu@gmail.com>
2023-10-30 14:22:15 +0900

    初期ルーティングとエラー処理機能の追加
    このコミットでは、ルートファイルとHelloWorldHandlerを追加することで、アプリケーションに基本的なルーティング機能を追加した。router.goの新しいメソッドGetRouterはHelloWorldHandlerをルート'/api/v1/hello-world'にアタッチし、ユーザーに基本的なナビゲーションシステムを提供する。
    
    さらに、アプリケーション全体で標準化されたエラーメッセージを提供するために使用できるカスタムエラー構造体がutils/my_error.goに追加された。これはエラーメッセージの扱い方と返し方を標準化し、読みやすさとデバッグのしやすさを向上させるために使うことができます。

--- src/main.go
+++ src/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
+	"github.com/kouhei-github/book-advertisement-site/route"
+	"github.com/kouhei-github/book-advertisement-site/utils/cors"
+
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("main")
+	// ルーターの設定
+	router := route.Router{Mutex: http.NewServeMux()}
+	router.GetRouter()
+
+	// CORS (Cross Origin Resource Sharing)の設定
+	// アクセスを許可するドメイン等を設定します
+	corsOrigin := cors.NewCorOrigin()
+
+	handler := corsOrigin.Handler(router.Mutex)
+	// Webサーバー起動時のエラーハンドリング => localhostの時コメントイン必要
+	if os.Getenv("ENVIRONMENT") == "local" {
+		if err := http.ListenAndServe(":8080", handler); err != nil {
+			panic(err)
+		}
+	}
+
+	// AWS Lambdaとの連携設定
+	lambda.Start(httpadapter.NewV2(handler).ProxyWithContext)
 }
